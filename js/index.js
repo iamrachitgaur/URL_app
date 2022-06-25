2630453bb8bb1b0935d674bdec7c1475b05995c5
Rachit Gaur <81481000+iamrachitgaur@users.noreply.github.com>
2022-06-25 13:18:51 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,2 @@
 
-const params = new URLSearchParams(window.location.search)
-const URL = params.get('URL')
-
-// const para = document.createElement('p');
-// para.innerHTML = URL;
-
-// const all_links = document.getElementById('all_links')
-
-// all_links.append(para)
-
 console.log("hello")
