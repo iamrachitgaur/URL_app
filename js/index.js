de58ff0bd91273e55a0df57e23a9c90567cc74d9
Rachit Gaur <81481000+iamrachitgaur@users.noreply.github.com>
2022-06-30 10:47:37 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,19 @@
-
 const params = new URLSearchParams(window.location.search)
 const URL = params.get('url')
 const gd = params.get('gd')
 
+const box = document.createElement('div');
 const para = document.createElement('p');
+const btn = document.createElement('btn');
+
+box.id = "link_box";
+btn.id = "mail_btn";
+btn.innerHTML = "Send";
+
 para.innerHTML = URL+"&gd="+gd;
 
 const all_links = document.getElementById('all_links')
 
-all_links.append(para)
+all_links.append(box)
+box.append(para)
+box.append(btn)
