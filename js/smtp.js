80d52cfae609515a97f99fc9c2c03a26018147ab
Rachit Gaur <81481000+iamrachitgaur@users.noreply.github.com>
2022-06-30 11:10:26 +0530

    Update smtp.js

--- js/smtp.js
+++ js/smtp.js
@@ -4,7 +4,7 @@ const mail_btn = document.getElementById('mail_btn');
 
 mail_btn.addEventListener('click',()=>{
     Email.send({
-        SecureToken : "2335edea-1a0c-423b-aacc-66b5829bd179",
+        SecureToken : "f692368d-f87b-4734-887c-fcefcd0690d6",
         To : 'rachitgaur3@gmail.com',
         From : "starlucifer808@gmail.com",
         Subject : "This is the subject",
